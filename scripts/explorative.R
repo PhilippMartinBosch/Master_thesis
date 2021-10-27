a707f8cfe45ea46128edff78ca11a6da8b47cc54
PhilippMartinBosch <philipp.bosch@uni-konstanz.de>
2021-10-27 13:33:43 +0200

    first commits

---
+++ scripts/explorative.R
@@ -0,0 +1,18 @@
+### first data explo ###
+
+
+# packages ----------------------------------------------------------------
+
+source(file = "scripts/packages.R")
+
+
+# read in data ------------------------------------------------------------
+
+
+smartphone_df <- read_tsv(file = "data/PollingPlaces2016_w_TimeZones_and_Buildings.tab") 
+
+cces_16 <- read_dta("data/CCES16_Common_OUTPUT_Feb2018_VV.dta")
+
+
+
+glimpse(cces_16)
